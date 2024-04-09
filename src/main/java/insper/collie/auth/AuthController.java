d66055abfb6d20f8632d6e85e94f3da7766f3f8e
alanmath <alanmatheus187@gmail.com>
2024-04-09 00:24:42 -0300

    agr vai

--- src/main/java/insper/collie/auth/AuthController.java
+++ src/main/java/insper/collie/auth/AuthController.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "collie-auth")
 public interface AuthController {
@@ -15,7 +16,8 @@ public interface AuthController {
 
     @PostMapping("/auth/login")
     ResponseEntity<LoginOut> authenticate (
-        @RequestBody(required = true) CredentialIn in
+        @RequestBody(required = true) CredentialIn in,
+        @RequestHeader(required = false, name = "secret") String secret
     );
 
     @PostMapping("/auth/solve")
